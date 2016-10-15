09e6d47f53824eb772449ee74110f62d657b8b47
jodegreef <jodegreef@gmail.com>
2016-10-15 17:31:26 +0200

    fail again

--- MyWebApp.Tests/UnitTest1.cs
+++ MyWebApp.Tests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace MyWebApp.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            //throw new Exception("fail");
+            throw new Exception("fail");
         }
     }
 }
